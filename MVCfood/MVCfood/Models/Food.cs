7478f1798528fb0817ed282f366ab48bb5a07be8
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 13:40:06 -0400

    Solucion Completa

---
+++ MVCfood/MVCfood/Models/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCfood.Models
+{
+    public class Food
+    {
+        [Key]
+        public int foodID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Description ="Food Description")]
+        [StringLength(30,ErrorMessage ="solo 30 caracteres",MinimumLength =5)]
+        public string Description { get; set; }
+
+
+
+
+    }
+}
